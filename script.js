76f90f267bc997a55e86b9bd22d107b35fb00193
asp <amandasueputney@gmail.com>
2023-07-05 19:07:12 -0700

    updates game with more code and new page

--- script.js
+++ script.js
@@ -6,19 +6,34 @@
 
 /*----- CONSTANTS -----*/
 //create die face image lookup object
+const DIE_LOOKUP = {
+    1: 'die1.JPG',
+    2: 'die2.JPG',
+    3: 'die3.JPG',
+    4: 'die4.JPG',
+    5: 'die5.JPG',
+    6: 'die6.JPG'
+};
 
 /*----- app's state (variables) -----*/
-let scoreBoard;  // 0-S  1-N  2-A  3-K  4-E  5-TOTAL
+let scoreBoard;  // player 1 player 2  0-S  1-N  2-A  3-K  4-E  5-TOTAL
 
-let dieRoll;
+// let dieRoll;
+
+let turn;
 
 
 /*----- cached element references -----*/
-
+//points scored per roll
+const p1PointsElement = document.getElementsByClassName('p1score');
+const p2PointsElement = document.getElementsByClassName('p2score');
+const resetButton = document.getElementById('reset');
 
 
 /*----- event listeners -----*/
-
+document.getElementById('roll').addEventListener('click', handleRoll);
+document.getElementById('reset').addEventListener('click', init);
+document.getElementById('hold').addEventListener('click', handleTurn);
 
 /*----- functions -----*/
 init();
@@ -32,36 +47,58 @@ function init() {
     //   a:0,
     //   k:0,
     //   e:0
+    //total: 0,
     // };
     scoreBoard = {
-        p1: [0, 0, 0, 0, 0, 0],
-        p2: [0, 0, 0, 0, 0, 0]
+        p1Score: [0, 0, 0, 0, 0, 0],
+        p2Score: [0, 0, 0, 0, 0, 0]
     };
 
-    dieRoll = {
-        die1: [1, 2, 3, 4, 5, 6],
-        die2: [1, 2, 3, 4, 5, 6]
-    };
+    // dieRoll = {
+    //     die1: [1, 2, 3, 4, 5, 6],
+    //     die2: [1, 2, 3, 4, 5, 6]
+    // };
 
+    turn = 1;
 
-
+    winner = null;
 
     render();
 }
 
+function handleRoll() {
+    render();
+}
+
+function handleTurn() {
+
+}
+
+
 function renderScoreBoard() {
+    scoreBoard.forEach()
+
+    // p1PointsElement.src = DIE_LOOKUP[scoreBoard.p1];
+    // p1PointsElement.src = scoreBoard.p1;
+    // p2PointsElement.src = DIE_LOOKUP[scoreBoard.p2];
     //reduce array iterator
     //square bracket notation
     //connected to dom element set in cached element references and pir constant variable image look up and die roll result
 };
 
 function renderDieRoll() {
-
+    let rollDie1 = Math.floor(Math.random() * 6) + 1;
+    console.log(rollDie1);
+    let rollDie2 = Math.floor(Math.random() * 6) + 1;
+    console.log(rollDie2);
+    roll1 = rollDie1 + rollDie2;
+    console.log(roll1);
 };
 
+//need to insert score in scoreboard array
+//need to reduce all 
 
 function render() {
-    renderScoreBoard();
     renderDieRoll();
+    // renderScoreBoard();
 }
-
